d6149e3d246b402189bf1199541e71f9ea8e4b92
Richard <richard.be.jamin@gmail.com>
2015-10-03 11:48:33 -0500

    bootstrap starter template

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bootstrap Project Template</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header></header>
+	<nav></nav>
+	<main role="content" id="content">
+		
+
+
+
+
+
+
+	</main>
+	<footer></footer>
+	<script src="js/app.js"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($){
+// Add custom scripts here
+});
