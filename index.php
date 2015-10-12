edd10a33390a88818b1e5c07362133747b328db9
Richard Keller <richard.be.jamin@gmail.com>
2015-10-12 00:59:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Bootstrap Project Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
